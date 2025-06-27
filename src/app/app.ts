2444a3ab13f27240bc3c228823ee96a17ebde720
Ali Gamal <aligamaldakhly99@gmail.com>
2025-06-27 19:23:06 +0300

    added the wishlist class in the imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Test } from './components/test/test';
+import { Wishlist } from './components/wishlist/wishlist';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Test],
+  imports: [RouterOutlet, Test, Wishlist],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
