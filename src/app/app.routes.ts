9399a26dc7d561eade46ef1df84cccd415f47f06
Ali Gamal <aligamaldakhly99@gmail.com>
2025-06-29 02:47:24 +0300

    added routes for wishlist and movie-cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
+import { MovieCard } from './components/movie-card/movie-card';
+import { Wishlist } from './components/wishlist/wishlist';
 // import { MovieDetailsComponent } from '../app/components/movie-details/movie-details';
 
 
 export const routes: Routes = [
     //  { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '', component: MovieCard },
       { path: 'login', component: Login },
-  { path: 'register', component: Register },
+      { path: 'register', component: Register },
+      { path: 'wishlist', component: Wishlist },
   // { path: 'movie/:id', component: MovieDetailsComponent },
   // { path: '', redirectTo: 'movie/1', pathMatch: 'full' } // مؤقتًا
 
