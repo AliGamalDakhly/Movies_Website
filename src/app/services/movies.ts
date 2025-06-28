d572c27db44af464471a054254a6eb741d0e6ac9
Ali Gamal <aligamaldakhly99@gmail.com>
2025-06-28 18:44:53 +0300

    added get movie by Id function.

--- src/app/services/movies.ts
+++ src/app/services/movies.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { MovieList } from '../Models/movie-list';
 import { MovieItem } from '../Models/movie-item';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,7 @@ export class Movies {
   HttpFetchData = inject(HttpClient);
   searchBaseURL:string = "https://api.themoviedb.org/3/search/movie?api_key=";
   MovieByName: MovieItem[] = [];
+  MovieById: MovieItem | null = null;
   //create signal
   moviesSignal = signal<MovieItem[]>([]);
   imagesBaseURL = "https://image.tmdb.org/t/p/w500";
@@ -36,6 +38,11 @@ export class Movies {
       }
     )
   }
+
+  getMovieById(Id: number): Observable<MovieItem> {
+  const url = `https://api.themoviedb.org/3/movie/${Id}?api_key=${environment.firebaseConfig.apiKey}`;
+  return this.HttpFetchData.get<MovieItem>(url);
+}
 
   
 
