d7bc948dbb6ca9121fede0a623581e7937910bf8
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-02 13:12:14 +0300

    updated registeration function.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,27 +1,55 @@
 import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, User, updateProfile } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from '@angular/fire/auth';
 import { Observable, from } from 'rxjs';
+import { User } from '../Models/user';
+import { Firebase } from './firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth, private firebaseService: Firebase) { }
+
+
+
+  register(email: string, password: string, firstName: string, lastName: string): Observable<any> {
+  return from(
+    createUserWithEmailAndPassword(this.auth, email, password).then(async (userCredential) => {
+      const user = userCredential.user;
+      const uid = user.uid;
+
+      await updateProfile(user, {
+        displayName: `${firstName} ${lastName}`
+      });
+
+      const newUser: Omit<User, 'id'> = {
+        FirstName: `${firstName}`,
+        LastName: `${lastName}`,
+        Email: email,
+        Wishlist: []
+      };
+
+      await this.firebaseService.createUserInFirestore(uid, newUser);
+
+      return userCredential;
+    })
+  );
+}
 
   // Register new user
-  register(email: string, password: string, firstName: string, lastName: string): Observable<any> {
-    return from(
-      createUserWithEmailAndPassword(this.auth, email, password).then((userCredential) => {
-        // Update user profile with first and last name
-        return updateProfile(userCredential.user, {
-          displayName: `${firstName} ${lastName}`
-        }).then(() => {
-          return userCredential;
-        });
-      })
-    );
-  }
+  // register(email: string, password: string, firstName: string, lastName: string): Observable<any> {
+  //   return from(
+  //     createUserWithEmailAndPassword(this.auth, email, password).then((userCredential) => {
+  //       // Update user profile with first and last name
+  //       return updateProfile(userCredential.user, {
+  //         displayName: `${firstName} ${lastName}`
+  //       }).then(() => {
+  //         return userCredential;
+  //       });
+  //     })
+  //   );
+  // }
 
   // Login user
   login(email: string, password: string): Observable<any> {
@@ -34,9 +62,9 @@ export class AuthService {
   }
 
   // Get current user
-  getCurrentUser(): User | null {
-    return this.auth.currentUser;
-  }
+  // getCurrentUser(): User | null {
+  //   return this.auth.currentUser;
+  // }
 
   // Check if user is authenticated
   isAuthenticated(): boolean {
