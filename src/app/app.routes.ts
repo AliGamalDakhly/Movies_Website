9d9cc98f9ddf1c35296567f7d97be8d67b036602
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-02 03:33:45 +0300

    added Not Found Page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Navbar } from './components/navbar/navbar';
 import { MovieCard } from './components/movie-card/movie-card';
 import { Wishlist } from './components/wishlist/wishlist';
 import { MovieDetails } from './components/movie-details/movie-details';
+import { PageNotFound } from './components/page-not-found/page-not-found';
 
 
 export const routes: Routes = [
@@ -17,6 +18,8 @@ export const routes: Routes = [
       { path: 'register', component: Register },
       { path: 'wishlist', component: Wishlist },
       { path: 'movie/:id', component: MovieDetails },
+      { path: '**', component: PageNotFound }
+
 ];
 
 @NgModule({
