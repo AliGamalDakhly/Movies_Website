8e61cbcd3d134e1bfab4f28ad7dbd741036c22d7
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-02 03:36:16 +0300

    added loading spinner

--- src/app/components/wishlist/wishlist.ts
+++ src/app/components/wishlist/wishlist.ts
@@ -1,10 +1,11 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieItem } from '../../Models/movie-item';
 import { Firebase } from '../../services/firebase';
 import { Movies } from '../../services/movies';
 import { User } from '../../Models/user';
 import { RouterLink } from '@angular/router';
+import { Language } from '../../services/language';
 
 type StarType = 'full' | 'half' | 'empty';
 
@@ -14,26 +15,63 @@ type StarType = 'full' | 'half' | 'empty';
   templateUrl: './wishlist.html',
   styleUrl: './wishlist.css'
 })
-export class Wishlist {
+export class Wishlist{
 
   tempwish: MovieItem[] = [];
   currentUser: User| null = null;
   loading: boolean = true;
 
-  constructor(private firebaseService: Firebase,private movieService: Movies)
+  constructor(private firebaseService: Firebase,private movieService: Movies,private languageService: Language)
   {
 
   }
     
-  async ngOnInit() 
-  {
-      this.loading = true;
-      await this.firebaseService.Init(); 
-      this.tempwish = this.firebaseService.wishlist;
-      this.currentUser = this.firebaseService.currentUser;
-      this.loading = false;
+ async ngOnInit() {
+  this.loading = true;
+
+  await this.firebaseService.Init(); 
+  this.currentUser = this.firebaseService.currentUser;
+
+  // أول مرة تحمل القائمة باللغة الحالية
+  this.updateWishlistByLanguage(this.languageService.currentLanguage);
+
+  // اشتراك على أي تغيير في اللغة
+  this.languageService.language$.subscribe((newLang) => {
+    this.updateWishlistByLanguage(newLang);
+  });
+
+  this.loading = false;
+}
+
+updateWishlistByLanguage(lang: string) {
+  this.tempwish = [];
+
+  if (this.currentUser) {
+    for (const movieId of this.currentUser.Wishlist) {
+      this.movieService.getMovieById(movieId, lang).subscribe(movie => {
+        movie.poster_path = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        this.tempwish.push(movie);
+      });
+    }
   }
+}
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   this.tempwish = [];
+
+  //   if(this.currentUser)
+  //   this.currentUser.Wishlist.forEach(element => {
+  //      this.movieService.getMovieById(element, this.languageService.currentLanguage).subscribe(
+  //       movie => {
+  //       this.tempwish.push(movie); // 👉 هنا بنضيف الفيلم للـ tempwish
+  //     }
+  //      )
+  //   });
     
+  // }
+
+
+   
 
   getStarsFromRating(rating: number): StarType[] 
   {
