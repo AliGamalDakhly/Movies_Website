06e629e13031a9533bd707afa3f6639af9627d03
Mostafa Mohamed <Mostafa2001Mohamed2001@gmail.com>
2025-07-16 23:25:28 +0300

    Update login.ts

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../../services/auth.service'; // Adjust path as nee
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink,ReactiveFormsModule, ,
+  imports: [RouterLink,ReactiveFormsModule,CommonModule]
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
